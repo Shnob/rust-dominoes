ca36cb86ecd3522b0b7c1c5ffdc42958f4ad2c6f
Shnob <jake.dombnz@gmail.com>
2023-04-09 23:02:18 +1200

    Single domino falling

--- src/domino.rs
+++ src/domino.rs
@@ -1,14 +1,45 @@
+use nannou::prelude::*;
+
 pub enum DominoState {
     Up,
-    Falling,
-    Down
+    Fall,
+    Down,
 }
 
 pub struct Domino {
-    pos: (f32, f32),
-    dir: (f32, f32),
+    pos: Vec2,
+    dir: Vec2,
     width: f32,
     depth: f32,
     height: f32,
     state: DominoState,
+}
+
+impl Domino {
+    pub fn new(pos: Vec2, dir: Vec2, shape: (f32, f32, f32)) -> Domino {
+        Domino {pos: pos, dir: dir, width: shape.0, depth: shape.1, height: shape.2, state: DominoState::Up}
+    }
+
+    pub fn knock(&mut self) {
+        self.state = DominoState::Fall;
+    }
+
+    pub fn update(&mut self) {
+        match self.state {
+            DominoState::Fall => self.state = DominoState::Down,
+            _ => (),
+        }
+    }
+
+    pub fn show(&self, draw: &Draw) {
+        let rad = self.dir.angle() - PI / 2.0;
+        match self.state {
+            DominoState::Up | DominoState::Fall => {
+                draw.rect().xy(self.pos).w_h(self.width, self.depth).rotate(rad).no_fill().stroke(WHITE).stroke_weight(1.0);
+            }
+            DominoState::Down => {
+                draw.rect().xy(self.pos).w_h(self.width, self.height).rotate(rad).no_fill().stroke(WHITE).stroke_weight(1.0);
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use nannou::prelude::*;
 mod domino;
 use domino::{Domino, DominoState};
 
+const DOMINO_SHAPE: (f32, f32, f32) = (10.0, 3.0, 20.0);
+
 fn main() {
     nannou::app(model)
         .update(update)
@@ -10,24 +12,45 @@ fn main() {
         .run();
 }
 
-struct Model {}
+struct Model {
+    dominoes: Vec<Domino>,
+}
 
 fn model(app: &App) -> Model {
     app.new_window()
         .size(600, 600)
+        .key_pressed(key_pressed)
         .build()
         .unwrap();
-    Model {}
+    Model {
+        dominoes: vec![
+            Domino::new(vec2(0.0, 0.0), vec2(0.0, 1.0), DOMINO_SHAPE),
+        ]
+    }
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
+fn key_pressed(_app: &App, model: &mut Model, key: Key) {
+    match key {
+        Key::Space => model.dominoes.get_mut(0).unwrap().knock(),
+        _ => (),
+    }
 }
 
-fn view(app: &App, _model: &Model, frame: Frame) {
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    for domino in model.dominoes.iter_mut() {
+        domino.update();
+    }
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     let _win = app.window_rect();
 
-    draw.background().color(RED);
+    draw.background().color(BLACK);
+
+    for domino in model.dominoes.iter() {
+        domino.show(&draw);
+    }
 
     draw.to_frame(&app, &frame).unwrap();
 }
